717ba6a7cc6d409a643ad14d21aa0be4e961ea4e
SJuras <jurasstpko@gmailcom>
2022-02-21 07:48:30 +0100

    v1.1.4 working on homepage.

--- zul-coin/pages/index.js
+++ zul-coin/pages/index.js
@@ -4,12 +4,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
+import { FaLanguage } from "react-icons/fa";
+
 export default function Home() {
 
   const [introLang, setIntroLang] = useState(1);
 
   return (
-    <div className=" w-full bg-blue-300">
+    <div className=" w-full bg-customLightBlue">
       <Head>
         <title>Zul Coin | Crypto</title>
         <meta name="description" content="Zul coin selling website, Web3 token" />
@@ -21,7 +23,7 @@ export default function Home() {
         <nav className="w-full mb-4 absolute z-50 top-0">
           <div className="flex justify-between items-center w-full max-w-7xl mx-auto p-3 mt-4">
             <div className="">
-                <h1 className="font-[Audiowide] text-2xl md:text-4xl text-customYellow drop-shadow-lg drop-shadow-lg"><span className="font-[Atari] text-4xl md:text-8xl text-customYellow drop-shadow-lg">Z</span>ULCOIN</h1>
+                <h1 className="font-[Audiowide] text-2xl md:text-4xl text-customWhite drop-shadow-lg drop-shadow-lg"><span className="font-[Atari] text-4xl md:text-8xl text-customWhite drop-shadow-lg">Z</span>ULCOIN</h1>
             </div>
             <div className="hidden p-2 bg-purple-500 font-[Starduster] text-xl text-yellow-300 cursor-pointer drop-shadow-lg">
               <button className="font-[Starduster] cursor-pointer">
@@ -36,17 +38,18 @@ export default function Home() {
           <div className="w-full max-w-7xl mx-auto flex flex-col md:flex-row h-screen justify-center items-center p-3">
             <div className="w-full md:w-1/2 flex flex-col justify-start items-center">
               <section className="w-full">
-                <h4 className="font-[Starduster] text-4xl md:text-8xl text-customYellow drop-shadow-lg">Secure your</h4>
-                <h4 className="font-[Starduster] text-4xl md:text-8xl text-customYellow drop-shadow-lg mb-4">future</h4>
-                <h1 className="text-2xl font-[Starduster] text-customYellow mb-8">MATIC Token of the future</h1>
+                <h4 className="font-[Starduster] text-4xl md:text-8xl text-customWhite drop-shadow-lg">invest</h4>
+                <h4 className="font-[Starduster] text-4xl md:text-8xl text-customWhite drop-shadow-lg">in your</h4>
+                <h4 className="font-[Starduster] text-4xl md:text-8xl text-customRed drop-shadow-lg mb-4">future</h4>
+                <h1 className="text-2xl font-[Starduster] text-customBlack mb-8">Polygon blockchAin token</h1>
               </section>
               <section className="flex w-1/2 md:w-full flex-col md:flex-row">
-                <div className="md:mr-4 p-2 bg-yellow-300 font-[Starduster] text-2xl cursor-pointer drop-shadow-lg transition duration-300 hover:bg-customLightPurple">
-                  <Link href="/lotterypage" className="font-[Starduster] cursor-pointer text-yellow-300">
-                    <p className="text-customYellow text-2xl text-center">Buy ZulCoin</p>
+                <div className="md:mr-4 p-2 bg-customRed font-[Starduster] text-2xl cursor-pointer drop-shadow-lg transition duration-300 hover:bg-customLightPurple">
+                  <Link href="/lotterypage" className="font-[Starduster] cursor-pointer text-customWhite">
+                    <p className="text-customWhite text-2xl text-center">Buy ZulCoin</p>
                   </Link>
                 </div>
-                <div className="text-center p-2 bg-customBlue font-[Starduster] text-2xl cursor-pointer drop-shadow-lg transition duration-300 hover:bg-customLightBlue">
+                <div className="text-center p-2 bg-customLightBlue font-[Starduster] text-2xl cursor-pointer drop-shadow-lg transition duration-300 hover:bg-customWhite">
                   <button href="/lotterypage" className="font-[Starduster] cursor-pointer text-customYellow">
                     LeArn More
                   </button>
@@ -77,15 +80,44 @@ export default function Home() {
           <div className="w-full max-w-7xl mx-auto flex justify-center items-center flex-col md:flex-row mb-4 bg-white rounded-xl border-4 border-customLightBlue drop-shadow-xl">
             <div className="w-full md:w-1/2 p-4">
               <div className="w-full">
-                <h4 className="font-[Starduster] text-xl">lAnguAge Selection</h4>
-                <div className="flex">
-                  <button className="p-2 px-4 mr-2 bg-customYellow text-customPurple font-bold rounded transition duration-300 hover:bg-customPurple hover:text-customYellow" onClick={() => setIntroLang(1)}>En</button>
-                  <button className="p-2 px-4 mr-2 bg-customYellow text-customPurple font-bold rounded transition duration-300 hover:bg-customPurple hover:text-customYellow" onClick={() => setIntroLang(2)}>Ja</button>
-                  <button className="p-2 px-4 mr-2 bg-customYellow text-customPurple font-bold rounded transition duration-300 hover:bg-customPurple hover:text-customYellow" onClick={() => setIntroLang(3)}>Hi</button>
-                  <button className="p-2 px-4 mr-2 bg-customYellow text-customPurple font-bold rounded transition duration-300 hover:bg-customPurple hover:text-customYellow" onClick={() => setIntroLang(4)}>Hu</button>
-                  <button className="p-2 px-4 mr-2 bg-customYellow text-customPurple font-bold rounded transition duration-300 hover:bg-customPurple hover:text-customYellow" onClick={() => setIntroLang(5)}>Tr</button>
+                <h4 className="font-[Starduster] text-xl"><FaLanguage className="inline text-4xl mr-2 text-customRed" />lAnguAge Selection</h4>
+                <div className="flex mb-2">
+                  <button className="p-2 px-4 mr-2 bg-customLightBlue text-customBlack font-bold rounded transition duration-300 hover:bg-customDark hover:text-customWhite" onClick={() => setIntroLang(1)}>En</button>
+                  <button className="p-2 px-4 mr-2 bg-customLightBlue text-customBlack font-bold rounded transition duration-300 hover:bg-customDark hover:text-customWhite" onClick={() => setIntroLang(2)}>Ja</button>
+                  <button className="p-2 px-4 mr-2 bg-customLightBlue text-customBlack font-bold rounded transition duration-300 hover:bg-customDark hover:text-customWhite" onClick={() => setIntroLang(3)}>Hi</button>
+                  <button className="p-2 px-4 mr-2 bg-customLightBlue text-customBlack font-bold rounded transition duration-300 hover:bg-customDark hover:text-customWhite" onClick={() => setIntroLang(4)}>Hu</button>
+                  <button className="p-2 px-4 mr-2 bg-customLightBlue text-customBlack font-bold rounded transition duration-300 hover:bg-customDark hover:text-customWhite" onClick={() => setIntroLang(5)}>Tr</button>
                 </div>
               </div>
+              { introLang == 1 &&
+                <div className="w-full">
+                  <h2 className="text-2xl font-[Starduster]">About ZulCoin</h2>
+                </div>
+              }
+
+              { introLang == 2 &&
+                <div className="w-full">
+                  <h2 className="text-2xl font-[Starduster] font-bold">ズルコインについて</h2>
+                </div>
+              }
+
+              { introLang == 3 &&
+                <div className="w-full">
+                  <h2 className="text-2xl font-[Starduster] font-bold">ज़ज़ुल्कोइन के बारे मेें</h2>
+                </div>
+              }
+
+              { introLang == 4 &&
+                <div className="w-full">
+                  <h2 className="text-2xl font-[Starduster]">A zulcoinról</h2>
+                </div>
+              }
+
+              { introLang == 5 &&
+                <div className="w-full">
+                  <h2 className="text-2xl font-[Starduster]">zulcoin hAkkindA</h2>
+                </div>
+              }
             </div>
             <div className="w-full md:w-1/2 p-4">
 
@@ -93,6 +125,9 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+
+
 
     </div>
   )
--- zul-coin/tailwind.config.js
+++ zul-coin/tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
       fontFamily: {
         starduster: ["Starduster", "sans-serif"],
       },
+      colors: {
+        customWhite: '#f7f3ed',
+        customRed: '#db262f',
+        customDark: '#14353a',
+        customLightBlue: '#97b5b2',
+        customBlack: '#1d2021',
+      },
     },
   },
   plugins: [],
